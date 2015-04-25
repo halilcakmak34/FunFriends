7d4d50acd7286b0990c565af49c31f2620b2635d
Halil Çakmak <halilcakmak34@gmail.com>
2015-04-25 19:48:00 +0300

    FunFriends Projesi ilklendirildi.

---
+++ application/views/welcome_message.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter Demo site</title>
+
+	<style type="text/css">
+
+	::selection{ background-color: #E13300; color: white; }
+	::moz-selection{ background-color: #E13300; color: white; }
+	::webkit-selection{ background-color: #E13300; color: white; }
+
+	body {
+		background-color: #fff;
+		margin: 40px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+
+	a {
+		color: #003399;
+		background-color: transparent;
+		font-weight: normal;
+	}
+
+	h1 {
+		color: #444;
+		background-color: transparent;
+		border-bottom: 1px solid #D0D0D0;
+		font-size: 19px;
+		font-weight: normal;
+		margin: 0 0 14px 0;
+		padding: 14px 15px 10px 15px;
+	}
+
+	code {
+		font-family: Consolas, Monaco, Courier New, Courier, monospace;
+		font-size: 12px;
+		background-color: #f9f9f9;
+		border: 1px solid #D0D0D0;
+		color: #002166;
+		display: block;
+		margin: 14px 0 14px 0;
+		padding: 12px 10px 12px 10px;
+	}
+
+	#body{
+		margin: 0 15px 0 15px;
+	}
+	
+	p.footer{
+		text-align: right;
+		font-size: 11px;
+		border-top: 1px solid #D0D0D0;
+		line-height: 32px;
+		padding: 0 10px 0 10px;
+		margin: 20px 0 0 0;
+	}
+	
+	#container{
+		margin: 10px;
+		border: 1px solid #D0D0D0;
+		-webkit-box-shadow: 0 0 8px #D0D0D0;
+	}
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to CodeIgniter Chat section</h1>
+
+	<div id="body">
+		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+		<p>If you would like to implement chat process,you can use following credintial to login.Use to different web browsers to how to working it. </p>
+		
+        <table width="500" border="1" cellspacing="1" cellpadding="1">
+  <tr>
+    <th scope="col">User name</th>
+    <th scope="col">Password</th>
+  </tr>
+  <tr>
+    <td>Smith</td>
+    <td>12345</td>
+  </tr>
+  <tr>
+    <td>Ann</td>
+    <td>11111</td>
+  </tr>
+  <tr>
+    <td>Chris</td>
+    <td>demo</td>
+  </tr>
+</table>
+
+     <br/>
+<br/>
+
+<a href="http://localhost/CodeIgniter/index.php/users">Users Login</a><br/>
+
+        
+        
+
+		<p><a href="http://webexplorar.com/codeigniter-chat-example/">Back to the CodeIgniter chat example Tutorial</a>.</p>
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+</div>
+
+</body>
+</html>
